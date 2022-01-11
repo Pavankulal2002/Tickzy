2b414c9bb437601ab9bc7a75a5bfa74067376d2d
Pavan <96732038+Pavankulal2002@users.noreply.github.com>
2022-01-11 23:31:18 +0530

    booking update

--- User/book.php
+++ User/book.php
@@ -13,17 +13,19 @@
 	// and inserts the data in the database accordingly
 	if(isset($_POST['submit']))
 	{
-		// Store the Product name in a "name" variable
-		$date = mysqli_real_escape_string($conn,$_POST['date']);
+		
 		
 		// Store the Category ID in a "id" variable
 		$match = mysqli_real_escape_string($conn,$_POST['match1']);
+		$date = mysqli_real_escape_string($conn,$_POST['date']);
+		$accomodation = mysqli_real_escape_string($conn,$_POST['accomodation']);
+		
 		
 		// Creating an insert query using SQL syntax and
 		// storing it in a variable.
 		$sql_insert =
-		"INSERT INTO `book`(`date`, `match1`)
-			VALUES ('$date','$match')";
+		"INSERT INTO `book`(`date`, `match1`,`accomodation`)
+			VALUES ('$date','$match','$accomodation')";
 		
 		// The following code attempts to execute the SQL query
 		// if the query executes with no errors
@@ -34,6 +36,39 @@
 			echo '<script>alert("booking added successfully")</script>';
 		}
 	}
+?>
+
+
+<?php
+
+	// Connect to database
+	$conn = mysqli_connect("localhost","root","","tickzy");
+	
+	// mysqli_connect("servername","username","password","database_name")
+
+	// Get all the categories from category table
+	$sql2 = "SELECT `date` FROM `matches` WHERE `match`= 'Aus vs Eng' ";
+	$all_categories2 = mysqli_query($conn,$sql2);
+
+	// The following code checks if the submit button is clicked
+	// and inserts the data in the database accordingly
+
+?>
+
+<?php
+
+	// Connect to database
+	$conn = mysqli_connect("localhost","root","","tickzy");
+	
+	// mysqli_connect("servername","username","password","database_name")
+
+	// Get all the categories from category table
+	$sql1 = "SELECT * FROM `accomodation`";
+	$all_categories1 = mysqli_query($conn,$sql1);
+
+	// The following code checks if the submit button is clicked
+	// and inserts the data in the database accordingly
+
 ?>
 
 
@@ -62,7 +97,7 @@
 				?>
 					<option value="<?php echo $category["match"];
 						// The value we usually set is the primary key
-					?>">
+					?>" >
 						<?php echo $category["match"];
 
 						echo constant ("space");
@@ -76,6 +111,63 @@
 				?>
 			</select>
 		</div>
+
+
+		<div>
+			<label > select the date</label>
+			<select name="date">
+				<?php
+
+					
+					// use a while loop to fetch data
+					// from the $all_categories variable
+					// and individually display as an option
+					while ($category2 = mysqli_fetch_array(
+							$all_categories2,MYSQLI_ASSOC)):;
+				?>
+					<option value="<?php echo $category2["date"];
+						// The value we usually set is the primary key
+					?>" >
+						<?php echo $category2["date"];
+
+					
+					?>
+					</option>
+				<?php
+					endwhile;
+					// While loop must be terminated
+				?>
+			</select>
+
+		</div>
+
+		<div>
+			<label > select the accomaodation</label>
+			<select name="accomodation">
+				<?php
+
+					define("space","  -  ");
+					// use a while loop to fetch data
+					// from the $all_categories variable
+					// and individually display as an option
+					while ($category1 = mysqli_fetch_array(
+							$all_categories1,MYSQLI_ASSOC)):;
+				?>
+					<option value="<?php echo $category1["acc_type"];
+						// The value we usually set is the primary key
+					?>" >
+						<?php echo $category1["acc_type"];
+
+						echo constant ("space");
+						echo $category1["acc_price"];
+							// To show the category name to the user
+					?>
+					</option>
+				<?php
+					endwhile;
+					// While loop must be terminated
+				?>
+			</select>
 		<br>
 		<input type="submit" value="submit" name="submit">
 	</form>
