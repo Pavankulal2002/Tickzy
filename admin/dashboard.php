1cdac0ed50e0f2b09a9921e0348a9fc7d1c1e0e3
Pavan <96732038+Pavankulal2002@users.noreply.github.com>
2022-01-16 22:20:43 +0530

    1

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" type="text/css"
     href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="dashboard.css">
+
+   
+
 </head>    
 <style>
     .flex-container1 {
--- admin/logout.php
+++ admin/logout.php
@@ -1,10 +1,23 @@
+<!-- <script type="text/javascript">
+      window.history.forward();
+      function noBack() {
+          window.history.forward();
+      }
+   </script> -->
+
+   
 
 <?php 
 if(session_status() == PHP_SESSION_NONE)
 {
 	session_start();//start session if session not start
+
 }
 
-unset($_SESSION['logged']);
+
+session_destroy();
+
+
+// unset($_SESSION['logged']);
 
 header('Location: ../Pages/home.html');
